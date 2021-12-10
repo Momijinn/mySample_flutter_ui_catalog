f2584646354076ef26287a92f5ea67902a04ec19
KanameTakano <momijinn.aka@gmail.com>
2021-12-11 07:20:18 +0900

    add widget

---
+++ my_sample_storybook_flutter/lib/pages/main_page/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:my_sample_storybook_flutter/widgets/my_card/my_card.dart';
+import 'package:my_sample_storybook_flutter/widgets/my_text/my_text.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            MyText(
+              param: 'hello',
+            ),
+            MyCard()
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ my_sample_storybook_flutter/lib/pages/main_page/main_page_widgetbook.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:my_sample_storybook_flutter/pages/main_page/main_page.dart';
+
+Widget mainPageUseCase(BuildContext context) {
+  return const MainPage();
+}
